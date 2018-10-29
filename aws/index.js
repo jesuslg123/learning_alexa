38e02cba40fe609fa5763ccb6707efe062b246be
Jesus Lopez Garcia <jesus@Jesuss-MacBook-Air.local>
2018-10-29 08:06:01 +0100

    New sample code

--- aws/index.js
+++ aws/index.js
@@ -1,151 +1,174 @@
-'use strict';
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
 
-// Route the incoming request based on type (LaunchRequest, IntentRequest,
-// etc.) The JSON body of the request is provided in the event parameter.
-exports.handler = function (event, context) {
-    try {
-        console.log("event.session.application.applicationId=" + event.session.application.applicationId);
+const Alexa = require('ask-sdk');
 
-        if (event.session.new) {
-            console.log("Session ON");
-            onSessionStarted({requestId: event.request.requestId}, event.session);
-        }
+//=========================================================================================================================================
+//TODO: The items below this comment need your attention.
+//=========================================================================================================================================
 
-        if (event.request.type === "LaunchRequest") {
-            console.log("LaunchRequest");
-            onLaunch(event.request,
-                event.session,
-                function callback(sessionAttributes, speechletResponse) {
-                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                });
-        } else if (event.request.type === "IntentRequest") {
-            console.log("IntentRequest");
-            onIntent(event.request,
-                event.session,
-                function callback(sessionAttributes, speechletResponse) {
-                    context.succeed(buildResponse(sessionAttributes, speechletResponse));
-                });
-        } else if (event.request.type === "SessionEndedRequest") {
-            console.log("SessionEndedRequest");
-            onSessionEnded(event.request, event.session);
-            context.succeed();
-        }
-    } catch (e) {
-        console.log("FAIL", e);
-        context.fail("Exception: " + e);
-    }
+const SKILL_NAME = 'Datos del espacio';
+const WELCOME_MESSAGE = '¡Bienvenido!';
+const GET_FACT_MESSAGE = 'Aquí está tu dato. ';
+const GET_STOP_MESSAGE = 'Parada ';
+const HELP_MESSAGE = 'Puedes pedirme un dato del espacio, o, puedes decir para... ¿Como te puedo ayudar?';
+const HELP_REPROMPT = '¿Cómo te puedo ayudar?';
+const STOP_MESSAGE = 'Hasta pronto';
+
+//=========================================================================================================================================
+//TODO: Replace this data with your own.  You can find translations of this data at http://github.com/alexa/skill-sample-node-js-fact/lambda/data
+//=========================================================================================================================================
+
+const data = [
+  'Las actuales naves espaciales tripuladas tardarían 700 siglos en llegar a las estrellas más cercanas.',
+  'Los rayos pueden llegar a medir 48 km de largo, su espesor es menor que una pulgada y pueden alcanzar una temperatura equivalente la superficie del Sol, en cuyo núcleo (el del Sol) se consumen unos 600 millones de toneladas de hidrógeno, que se convierten en helio. Sobre la Tierra impactan alrededor de 2000 rayos provenientes de tormentas eléctricas. Un solo rayo puede liberar más de 1 millón de vatios de electricidad (más o menos 1,21 GigaVatios… lo necesario para que el Delorean consiga viajar en el tiempo).',
+  'La luna está cuatrocientas veces más cerca de la Tierra que el sol, y es exactamente cuatrocientas veces más pequeña que este.',
+  'Existen más de 10000 objetos elaborados por el hombre con un tamaño mayor que el de un pomelo orbitando a la Tierra. La basura espacial viaja a través del espacio a 29000 km por hora.',
+  'La Estación Espacial Internacional pesa cerca de 500 toneladas y tiene las dimensiones de un campo de fútbol.',
+  'Los astronautas trajeron aproximadamente 362 kg de rocas lunares a la Tierra, la mayoría de las cuales no han sido aun analizadas.',
+  'Saturno flotaría si se pudiera encontrar un océano lo suficientemente grande.',
+  'Un pedazo de una estrella de neutrones del tamaño de una cabeza de alfiler pesaría un millón de toneladas. Una estrella de neutrones cuyo diámetro fuese de 24 km pesaría más que el sol. Son tan densas que una sola cucharadita sería más pesada que toda la población terrestre.',
+  'La sonda de investigación Helio Bé, se acercó al sol a la distancia record de 43,45 millones de kilómetros.',
+  'Se cree que hace 65 millones de años, el impacto de un asteroide liberó la energía equivalente a 10 millones de bombas H y causó la extinción de la mayoría de las especies de dinosaurios.',
+  'Se estima que la temperatura en el centro de la Tierra es de 5500 grados Celsius.',
+  'En la actualidad más de 4000 satélites orbitan nuestro planeta.',
+  'Venus es así de brillante a causa de su gruesa capa de nubes que refleja los rayos del sol.',
+  'En nuestra galaxia, la Vía Láctea, hay más de 100000 millones de estrellas.',
+  'Los científicos han descubierto alrededor de doscientos planetas fuera de nuestro sistema solar.',
+  'El sol tarda aproximadamente 220 millones de años en completar una vuelta alrededor de la vía láctea.',
+  'Normalmente se redondea la velocidad de la luz entorno a los 300000 km por segundo. La medición exacta es de 299792458 metros por segundo.',
+  'La Tierra rota a una velocidad de 1609 km por hora, pero se desplaza a través del espacio a la increíble velocidad de 107826 km por hora.',
+  'Los astronautas no pueden eructar porque la ingravidez no permite la separación de líquido y gas en sus estómagos.',
+  'Una millonésima de la millonésima de la millonésima de la millonésima de la millonésima de segundo después del Big Bang, el universo ya tenía el tamaño de un guisante.',
+  'Dentro de 5 mil millones de años el Sol se quedará sin combustible y se convertirá en una Gigante Roja.',
+  'Si el sol fuese del tamaño de un balón de playa, Júpiter tendría el tamaño de una pelota de golf y la Tierra sería tan solo un guisante. Si el Sol midiese tan solo una pulgada de diámetro (2,54 cm), la estrella más cercana se encontraría a 716 km de distancia.',
+  'Alrededor de 1000 billones de neutrinos del sol habrán atravesado tu cuerpo mientras escuchas esta frase.',
+  'El cohete Saturno 5 que llevó al hombre a la luna desarrollaba una energía equivalente a 50 aviones Boing 747.',
+  'Si cada estrella de la Vía Láctea tuviese el tamaño de un grano de sal, entre todas podrían llenar una piscina olímpica.'
+];
+
+//=========================================================================================================================================
+//Editing anything below this line might break your skill.
+//=========================================================================================================================================
+
+const LaunchHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+    const speechOutput = WELCOME_MESSAGE + ' ' + HELP_MESSAGE;
+
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .reprompt(speechOutput)
+      //.withShouldEndSession(false)
+      .withSimpleCard(SKILL_NAME, WELCOME_MESSAGE)
+      .getResponse();
+  },
 };
 
-/**
- * Called when the session starts.
- */
-function onSessionStarted(sessionStartedRequest, session) {
-    console.log("onSessionStarted requestId=" + sessionStartedRequest.requestId
-        + ", sessionId=" + session.sessionId);
+const ObtenerNuevoDatoHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest' && request.intent.name === 'ObtenerNuevoDatoIntent';
+  },
+  handle(handlerInput) {
+    let i = Math.floor(Math.random() * data.length);
+    const randomFact = data[i];
+    const speechOutput = GET_FACT_MESSAGE + randomFact;
 
-    // add any session init logic here
-}
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .reprompt(HELP_REPROMPT)
+      .withSimpleCard(SKILL_NAME, randomFact)
+      .getResponse();
+  },
+};
 
-/**
- * Called when the user invokes the skill without specifying what they want.
- */
-function onLaunch(launchRequest, session, callback) {
-    console.log("onLaunch requestId=" + launchRequest.requestId
-        + ", sessionId=" + session.sessionId);
+const StopNumberHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest' && request.intent.name === 'StopNumberIntent';
+  },
+  handle(handlerInput) {
+    console.log(handlerInput.requestEnvelope.request.intent);
+    let i = Math.floor(Math.random() * 30);
+    const stopInfo = handlerInput.requestEnvelope.request.intent.slots.stopNumber.value + ", la línea 22 llega en " + i + " minutos";
+    const speechOutput = GET_STOP_MESSAGE + stopInfo;
 
-    var cardTitle = "Hello, World!";
-    var speechOutput = "You can tell Hello, World! to say Hello, World!";
-    callback(session.attributes,
-        buildSpeechletResponse(cardTitle, speechOutput, "", true));
-}
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .reprompt(HELP_REPROMPT)
+      .withSimpleCard(SKILL_NAME, stopInfo)
+      .getResponse();
+  },
+};
 
-/**
- * Called when the user specifies an intent for this skill.
- */
-function onIntent(intentRequest, session, callback) {
-    console.log("onIntent requestId=" + intentRequest.requestId
-        + ", sessionId=" + session.sessionId);
+const HelpHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(HELP_MESSAGE)
+      .reprompt(HELP_REPROMPT)
+      .getResponse();
+  },
+};
 
-    var intent = intentRequest.intent,
-        intentName = intentRequest.intent.name;
+const ExitHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && (request.intent.name === 'AMAZON.CancelIntent'
+        || request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(STOP_MESSAGE)
+      .getResponse();
+  },
+};
 
-    console.log("intent: ", intentName);
-    console.log(intent);
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
 
-    // dispatch custom intents to handlers here
-    if (intentName == 'HelloWorldIntent') {
-        handleTestRequest(intent, session, callback);
-    } else if (intentName == 'StopIntent') {
-        handleStopRequest(intent, session, callback);
-    } else {
-        throw "Invalid intent";
-    }
-}
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
 
-/**
- * Called when the user ends the session.
- * Is not called when the skill returns shouldEndSession=true.
- */
-function onSessionEnded(sessionEndedRequest, session) {
-    console.log("onSessionEnded requestId=" + sessionEndedRequest.requestId
-        + ", sessionId=" + session.sessionId);
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
 
-    // Add any cleanup logic here
-}
+    return handlerInput.responseBuilder
+      .speak('Perdona, ha ocurrido un error.')
+      .reprompt('Perdona, ha ocurrido un error.')
+      .getResponse();
+  },
+};
 
-function handleTestRequest(intent, session, callback) {
-    callback(session.attributes,
-        buildSpeechletResponseWithoutCard("Hello, World!", "", "true"));
-}
+const skillBuilder = Alexa.SkillBuilders.custom();
 
-function handleStopRequest(intent, session, callback) {
-    callback(session.attributes,
-        buildSpeechletResponseWithoutCard("Informacion de parada numero: ", "", "true"));
-}
-
-// ------- Helper functions to build responses -------
-
-function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: "PlainText",
-            text: output
-        },
-        card: {
-            type: "Simple",
-            title: title,
-            content: output
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "PlainText",
-                text: repromptText
-            }
-        },
-        shouldEndSession: shouldEndSession
-    };
-}
-
-function buildSpeechletResponseWithoutCard(output, repromptText, shouldEndSession) {
-    return {
-        outputSpeech: {
-            type: "PlainText",
-            text: output
-        },
-        reprompt: {
-            outputSpeech: {
-                type: "PlainText",
-                text: repromptText
-            }
-        },
-        shouldEndSession: shouldEndSession
-    };
-}
-
-function buildResponse(sessionAttributes, speechletResponse) {
-    return {
-        version: "1.0",
-        sessionAttributes: sessionAttributes,
-        response: speechletResponse
-    };
-}
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    LaunchHandler,
+    ObtenerNuevoDatoHandler,
+    StopNumberHandler,
+    HelpHandler,
+    ExitHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
